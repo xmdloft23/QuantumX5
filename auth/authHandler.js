01f2180fb24fdbbbf6be7ce30fdf73fdefd1b08a
xmdloft23 <xmdloft745@gmail.com>
2025-10-02 13:20:32 +0300

    Add files via upload

---
+++ auth/authHandler.js
@@ -0,0 +1,79 @@
+import pkg from 'bailey';
+const { makeWASocket, useMultiFileAuthState, DisconnectReason} = pkg;
+
+
+import configManager from '../utils/manageConfigs.js';
+
+import readline from 'readline';
+
+import startSession from '../utils/connector.js'
+
+async function promptUserNumber() {
+
+    return new Promise((resolve) => {
+
+        const rl = readline.createInterface({
+
+            input: process.stdin,
+
+            output: process.stdout,
+
+        });
+
+        rl.question('', (number) => {
+
+            rl.close();
+
+            resolve(number.trim());
+        });
+    });
+}
+
+
+async function connectToWhatsApp(handleMessage) {
+
+    console.log(`
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣷⣮⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣻⣿⣿⣿⣿⣿⠂⠀⠀
+⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⢸⣧⠁⠀⠀⠀
+⠀⡀⠀⠀⠀⠀⢸⣿⣿⣿⣸⣿⣷⣄⠀⠀
+⠀⠈⠫⠂⠀⠀⠊⣿⢿⣿⡏⣿⠿⠟⠀⠀
+⠀⠀⠀⠀⠱⡀⠈⠁⠀⢝⢷⡸⡇⠀⠀⠀
+⠀⠀⠀⠀⢀⠇⠀⠀⢀⣾⣦⢳⡀⠀⠀⠀
+⠀⠀⠀⢀⠎⠀⢀⣴⣿⣿⣿⡇⣧⠀⠀⠀
+⠀⢀⡔⠁⠀⢠⡟⢻⡻⣿⣿⣿⣌⡀⠀⠀
+⢀⡎⠀⠀⠀⣼⠁⣼⣿⣦⠻⣿⣿⣷⡀⠀
+⢸⠀⠀⠀⠀⡟⢰⣿⣿⡟⠀⠘⢿⣿⣷⡀
+⠈⠳⠦⠴⠞⠀⢸⣿⣿⠁⠀⠀⠀⠹⣿⡧
+⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⢰⣿⡇
+⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⢸⣿⡇
+⠀⠀⠀⠀⠀⠀⢸⣿⠁⠀⠀⠀⠀⢸⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⣿⣆⠀⠀⠀⠀⠀⣿⣧
+⠀⠀⠀⠀⠀⠀⠀⠏⢿⠄⠀⠀⠀⠐⢸⣿
+
+    `);
+
+    console.log("📲 Enter your WhatsApp number (with country code, e.g., 255xxxx): ");
+
+    const primary = configManager.config?.users["root"]?.primary;
+
+    if (!primary) {
+
+        const number = await promptUserNumber();
+
+        await startSession(number, handleMessage, true);
+
+    } else {
+
+        const number = primary;
+
+        await startSession(number, handleMessage, false);
+    }
+  
+}
+
+
+export default connectToWhatsApp;

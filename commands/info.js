f77e9b7acedce43a6c4c94df7bc553ab28ade081
xmdloft23 <xmdloft745@gmail.com>
2025-10-03 12:25:21 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -136,19 +136,15 @@ export async function info(message, client) {
     });
 
     await client.sendMessage(remoteJid, {
-      audio: { url: "https://files.catbox.moe/ztn9bu.mp3" }, 
-      mimetype: 'audio/mpeg',
-      ptt: false,
-      contextInfo: {
-          externalAdReply: {
-              title: `${BOT_NAME}`,
-              body: "Audio with image",
-              mediaType: 2,
-              thumbnail: fs.readFileSync(imgPath),
-          }
-      },
-      quoted: message
-  });
+
+            audio: { url: "https://files.catbox.moe/ztn9bu.mp3" }, 
+
+            mimetype: 'audio/mpeg',
+
+            ptt: true,
+
+            quoted: message
+        });
 }   
 
 export default info;
